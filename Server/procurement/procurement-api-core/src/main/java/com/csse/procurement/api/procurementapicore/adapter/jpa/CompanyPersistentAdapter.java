656a752fe1c0cf671247ab9cf55b8015b365d600
007Miles <milendrafernando@gmail.com>
2023-10-15 20:52:34 +0530

    complete company implementation

--- Server/procurement/procurement-api-core/src/main/java/com/csse/procurement/api/procurementapicore/adapter/jpa/CompanyPersistentAdapter.java
+++ Server/procurement/procurement-api-core/src/main/java/com/csse/procurement/api/procurementapicore/adapter/jpa/CompanyPersistentAdapter.java
@@ -106,76 +106,142 @@ public class CompanyPersistentAdapter implements SaveCompanyPort, GetCompanyPort
 
     @Override
     public void createDepartment(Department department) {
-
+        if (department != null) {
+            com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Department dbDepartment = mapper.map(department, com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Department.class);
+            departmentRepository.save(dbDepartment);
+        }
     }
 
     @Override
     public void updateDepartment(Department department) {
+        Long departmentId = department.getId();
 
+        if (departmentId == null) {
+            return;
+        }
+
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Department> optionalDepartment = departmentRepository.findById(departmentId);
+        if (optionalDepartment.isPresent()) {
+            com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Department existingDepartment = optionalDepartment.get();
+
+            existingDepartment.setDepartmentName(department.getDepartmentName());
+            departmentRepository.save(existingDepartment);
+        }
     }
 
     @Override
     public void deleteDepartmentById(Long id) {
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Department> optionalDepartment = departmentRepository.findById(id);
 
+        if (optionalDepartment.isPresent()) {
+            departmentRepository.deleteById(id);
+        }
     }
 
     @Override
     public List<Department> getAllDepartments() {
-        return null;
+        return departmentRepository.findAll().stream().map(n->mapper.map(n, Department.class)).toList();
     }
 
     @Override
     public Department getDepartmentById(Long id) {
-        return null;
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Department> department = departmentRepository.findById(id);
+        return mapper.map(department, Department.class);
     }
 
     @Override
     public void createPolicy(Policy policy) {
-
+        if (policy != null) {
+            com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Policy dbPolicy = mapper.map(policy, com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Policy.class);
+            policyRepository.save(dbPolicy);
+        }
     }
 
     @Override
     public void updatePolicy(Policy policy) {
+        Long policyId = policy.getId();
 
+        if (policyId == null) {
+            return;
+        }
+
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Policy> optionalPolicy = policyRepository.findById(policyId);
+        if (optionalPolicy.isPresent()) {
+            com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Policy existingPolicy = optionalPolicy.get();
+            LineManager lineManager = lineManagerRepository.findById(policy.getMadeBy().getId()).orElse(null);
+
+            existingPolicy.setPolicyNo(policy.getPolicyNo());
+            existingPolicy.setStatements(policy.getStatements());
+            existingPolicy.setMadeBy(lineManager);
+            policyRepository.save(existingPolicy);
+        }
     }
 
     @Override
     public void deletePolicyById(Long id) {
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Policy> optionalPolicy = policyRepository.findById(id);
 
+        if (optionalPolicy.isPresent()) {
+            policyRepository.deleteById(id);
+        }
     }
 
     @Override
     public List<Policy> getAllPolicies() {
-        return null;
+        return policyRepository.findAll().stream().map(n->mapper.map(n, Policy.class)).toList();
     }
 
     @Override
     public Policy getPolicyById(Long id) {
-        return null;
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.Policy> policy = policyRepository.findById(id);
+        return mapper.map(policy, Policy.class);
     }
 
     @Override
     public void createBankAccount(BankAccount bankAccount) {
-
+        if (bankAccount != null) {
+            com.csse.procurement.api.procurementapicore.adapter.jpa.entity.BankAccount dbBankAccount = mapper.map(bankAccount, com.csse.procurement.api.procurementapicore.adapter.jpa.entity.BankAccount.class);
+            bankAccountRepository.save(dbBankAccount);
+        }
     }
 
     @Override
     public void updateBankAccount(BankAccount bankAccount) {
+        Long bankAccountId = bankAccount.getId();
 
+        if (bankAccountId == null) {
+            return;
+        }
+
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.BankAccount> optionalBankAccount = bankAccountRepository.findById(bankAccountId);
+        if (optionalBankAccount.isPresent()) {
+            com.csse.procurement.api.procurementapicore.adapter.jpa.entity.BankAccount existingBankAccount = optionalBankAccount.get();
+
+            existingBankAccount.setAccountNo(bankAccount.getAccountNo());
+            existingBankAccount.setAccountName(bankAccount.getAccountName());
+            existingBankAccount.setAccountType(bankAccount.getAccountType());
+            existingBankAccount.setBranch(bankAccount.getBranch());
+            bankAccountRepository.save(existingBankAccount);
+        }
     }
 
     @Override
     public void deleteBankAccountById(Long id) {
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.BankAccount> optionalBankAccount = bankAccountRepository.findById(id);
 
+        if (optionalBankAccount.isPresent()) {
+            bankAccountRepository.deleteById(id);
+        }
     }
 
     @Override
     public List<BankAccount> getAllBankAccounts() {
-        return null;
+        return bankAccountRepository.findAll().stream().map(n->mapper.map(n, BankAccount.class)).toList();
     }
 
     @Override
     public BankAccount getBankAccountById(Long id) {
-        return null;
+        Optional<com.csse.procurement.api.procurementapicore.adapter.jpa.entity.BankAccount> bankAccount = bankAccountRepository.findById(id);
+        return mapper.map(bankAccount, BankAccount.class);
     }
 }
