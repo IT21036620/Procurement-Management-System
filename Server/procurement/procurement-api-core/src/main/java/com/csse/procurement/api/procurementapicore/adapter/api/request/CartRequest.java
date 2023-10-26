139ad5e0da56c0598c1c52a6f16585ed0264a7b8
007Miles <milendrafernando@gmail.com>
2023-10-26 23:11:00 +0530

    fix: order and requisition update to patch

--- Server/procurement/procurement-api-core/src/main/java/com/csse/procurement/api/procurementapicore/adapter/api/request/CartRequest.java
+++ Server/procurement/procurement-api-core/src/main/java/com/csse/procurement/api/procurementapicore/adapter/api/request/CartRequest.java
@@ -3,7 +3,6 @@ package com.csse.procurement.api.procurementapicore.adapter.api.request;
 import com.csse.hexa.core.Request;
 import com.csse.procurement.business.entity.PurchaseOrderItem;
 import lombok.Getter;
-import org.apache.catalina.LifecycleState;
 
 import java.util.List;
 
